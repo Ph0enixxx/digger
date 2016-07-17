072065df8dc9777b998c2ab6a817e588b9670062
Ph0enixxx <754406613@qq.com>
2016-07-17 14:58:13 +0800

    修正部分bug

--- wjj.py
+++ wjj.py
@@ -1,10 +1,15 @@
-__author__ = 'AZONE'
+__author__ = 'AZONE&Mikoyan'
 import Queue
 import urllib2
 from threading import Thread
 import sys
 import httplib
 import urlparse
+
+#dirs = ["index.php","wwwroot.zip","wwwroot.rar","www.rar","www.zip","web.rar","web.zip","db.rar","db.zip","wz.rar","wz.zip","fdsa.rar","fdsa.zip","wangzhan.rar","wangzhan.zip","root.rar","root.zip","admin.rar","admin.zip","data.rar","gg.rar","vip.rar","1.zip","1.rar","2.zip","2.rar","config.rar","config.zip","/config/config.rar","/config/config.zip"]
+dirs =[i[:-1] for i in open('path.txt')]
+print dirs
+
 def bThread(taget):
     SETTHREAD = 800
     print '[Note] Running...\n'
@@ -43,29 +48,29 @@ for line in fd.readlines():
         taget.append("http://"+line.strip()+"/")
 print taget
 print "[Note] Thread:800"
-dirs = ["wwwroot.zip","wwwroot.rar","www.rar","www.zip","web.rar","web.zip","db.rar","db.zip","wz.rar","wz.zip","fdsa.rar","fdsa.zip","wangzhan.rar","wangzhan.zip","root.rar","root.zip","admin.rar","admin.zip","data.rar","gg.rar","vip.rar","1.zip","1.rar","2.zip","2.rar","config.rar","config.zip","/config/config.rar","/config/config.zip"]
 def requesturl(taget):
-    for i in range(29):
-        TURL = taget + dirs[i]
-        request = urllib2.Request(TURL)
+    for i in dirs:
+        TURL = taget + i
         try:
+            request = urllib2.Request(TURL)
             response = urllib2.urlopen(request)
             back = response.read()
-            #print "[%d] => %s" %(response.code,TURL)
+            print "[%d] => %s" %(response.code,TURL)
             response.close()
-            parsedurl = urlparse.urlparse(TURL)
+            """parsedurl = urlparse.urlparse(TURL)
             httpConn = httplib.HTTPConnection(parsedurl[1])
             httpConn.request('GET', parsedurl[2])
             responsed = httpConn.getresponse()
-            if responsed.status == 200:
+            if responsed.status != 404:
                 size = responsed.getheader('Content-Length')
                 size = int(size) / 1024
                 if size > 1024:
                     print TURL+'\n'
-                    print 'Size: %s KB' %size
+                    print 'Size: %s KB' %size"""
 
         except urllib2.HTTPError as error:
-            #print TURL+"ERROR!"
+            #print "[ERROR]"+TURL+"----"+error
+            #print 
             pass
 bThread(taget)
-sys.exit()
+#sys.exit()
